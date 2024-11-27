471f4ad316a4909891f5983fe3831d4dff2a50ca
Yash Shelar <ydshelar04@gmail.com>
2024-11-27 15:02:56 +0530

    New commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,62 @@
-import Navbar from "./components/Navbar";
-
-import HomePage from "./pages/HomePage";
-import SignUpPage from "./pages/SignUpPage";
-import LoginPage from "./pages/LoginPage";
-import SettingsPage from "./pages/SettingsPage";
-import ProfilePage from "./pages/ProfilePage";
-
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useAuthStore } from "./store/useAuthStore";
-import { useThemeStore } from "./store/useThemeStore";
-import { useEffect } from "react";
-
-import { Loader } from "lucide-react";
+import React from "react";
+import Left from "./home/Leftpart/Left";
+import Right from "./home/Rightpart/Right";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import { useAuth } from "./context/AuthProvider";
 import { Toaster } from "react-hot-toast";
+import Logout from "./home/left1/Logout";
 
-const App = () => {
-  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
-  const { theme } = useThemeStore();
-
-  console.log({ onlineUsers });
-
-  useEffect(() => {
-    checkAuth();
-  }, [checkAuth]);
-
-  console.log({ authUser });
-
-  if (isCheckingAuth && !authUser)
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <Loader className="size-10 animate-spin" />
-      </div>
-    );
-
+import { Navigate, Route, Routes } from "react-router-dom";
+function App() {
+  const [authUser, setAuthUser] = useAuth();
+  console.log(authUser);
   return (
-    <div data-theme={theme}>
-      <Navbar />
-
+    <>
       <Routes>
-        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
-        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
-      </Routes>
+        <Route
+          path="/"
+          element={
+            authUser ? (
+              <div className="flex h-screen">
+                <Logout />
+                <Left />
+                <Right />
+              </div>
 
+
+            
+            ) : (
+              <Navigate to={"/login"} />
+            )
+          }
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <Signup />}
+        />
+      </Routes>
       <Toaster />
-    </div>
+    </>
   );
-};
+}
+
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-
+import "./index.css";
+import { AuthProvider } from "./context/AuthProvider.jsx";
 import { BrowserRouter } from "react-router-dom";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+import { SocketProvider } from "./context/SocketContext.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <SocketProvider>
+        <App />
+      </SocketProvider>
+    </AuthProvider>
+  </BrowserRouter>
 );
